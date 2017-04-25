d8c0c567b2c18f0c4723192338fa3043861ead9f
adrian.kardasz <a-kardasz@wiu.edu>
2017-04-25 13:27:03 -0500

    API for registering users with mongodb and authentication using JWT (json web token). This app uses passport and passport-jwt and uses a JWT strategy

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to Database
+mongoose.connect(config.database);
+// On Connection, LOG
+mongoose.connection.on('connected', () => {
+  console.log('connected to database:' + config.database)
+})
+// On Error, LOG
+mongoose.connection.on('error', (err) => {
+  console.log('database error:' + err)
+})
+
+const app = express();
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+const users = require('./routes/users');
+
+// Port Number
+const port = 3000;
+
+// Cors Middleware
+app.use(cors());
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname,'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req,res) => {
+  res.send('Invalid endpoint')
+});
+// Start Server
+app.listen(port, () => {
+  console.log('Server started on port:'+port)
+});
